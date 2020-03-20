e132e98f316051671c7f37aa49a67612fd339faf
Surender Lohia <surenloh@thoughtworks.com>
2020-03-20 18:58:52 +0530

    Updates

---
+++ precache-manifest.3092cbbe473e93c50a359fd3f753d0e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8f85c3b994575ad74aeb0dc6d4395c",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "2ba37fe1663649ca0a08",
+    "url": "/trivia/static/css/2.5dabca3a.chunk.css"
+  },
+  {
+    "revision": "59bfecf7213a598241b5",
+    "url": "/trivia/static/css/main.7266cd7b.chunk.css"
+  },
+  {
+    "revision": "2ba37fe1663649ca0a08",
+    "url": "/trivia/static/js/2.fbd8eca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trivia/static/js/2.fbd8eca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bfecf7213a598241b5",
+    "url": "/trivia/static/js/main.9152723d.chunk.js"
+  },
+  {
+    "revision": "4a0429a3bd8d90900b62",
+    "url": "/trivia/static/js/runtime-main.a7052699.js"
+  }
+]);
