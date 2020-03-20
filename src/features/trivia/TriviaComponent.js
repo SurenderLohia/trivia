04441bad45a568fcc9d5c2c5458b129dca123d39
Surender Lohia <surenloh@thoughtworks.com>
2020-03-20 06:27:24 +0530

    Refactor

--- src/features/trivia/TriviaComponent.js
+++ src/features/trivia/TriviaComponent.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-function Trivia(props) {
-  const [triviaList, setTriviaList] = useState([]);
-  useEffect(() => {
-    console.log('useEffect');
-    const url = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
-    axios.get(url)
-      .then((response) => {
-        setTriviaList(response.data.results);
-      })
-
-    //console.log('props', props);
-  });
-
+function Trivia() {
   return (
     <div className="flex-item">
       <h3 className="is-size-6 has-text-centered">Round 1/20</h3>
-  <h1 className="is-size-1 has-text-centered mb1">0:07</h1>
+      <h1 className="is-size-1 has-text-centered mb1">0:07</h1>
       <div className="trivia-main-section mb2">
         <h2 className="is-size-3 question has-text-centered mb2">In which city, is the Big Nickel located in Canada?</h2>
         <div className="columns is-multiline is-variable is-7">
@@ -49,16 +35,6 @@ function Trivia(props) {
       </div>
     </div>
   )
-}
-
-Trivia.propTypes = {
-  fetchTriviaData: PropTypes.func,
-  triviaData: PropTypes.array,
-  name: PropTypes.string
-};
-
-Trivia.defaultPropTypes = {
-  triviaData: []
 }
 
 export default Trivia;
